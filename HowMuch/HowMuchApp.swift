375240356585b23d2765760ba5f4e590cf9cffba
wannabemountainking <kdy0096@gmail.com>
2026-06-17 15:40:32 +0900

    Initial Commit

---
+++ HowMuch/HowMuchApp.swift
@@ -0,0 +1,17 @@
+//
+//  HowMuchApp.swift
+//  HowMuch
+//
+//  Created by YoonieMac on 6/17/26.
+//
+
+import SwiftUI
+
+@main
+struct HowMuchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
