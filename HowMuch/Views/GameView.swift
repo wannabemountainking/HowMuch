1f79f6d1312fd278ee7c1e85c3e62752d2789d12
김도윤 <kdy0096@gmail.com>
2026-06-21 00:19:46 +0900

    GameView additional

--- HowMuch/Views/GameView.swift
+++ HowMuch/Views/GameView.swift
@@ -11,12 +11,12 @@ import SwiftUI
 /// 문제 풀기 화면
 struct GameView: View {
 	
-	@Environment(GameViewModel.self) private var vm
+    @Environment(GameViewModel.self) private var vm
 		
     var body: some View {
 		GeometryReader { geometry in
 			VStack(spacing: 0) {
-				HeaderView()
+                HeaderView(geo: geometry)
 					.frame(height: geometry.size.height * ComponentRatio.headerHeight)
 					.background(Color(hex: HexColors.gameHeader))
 				QuestionView()
@@ -28,11 +28,65 @@ struct GameView: View {
     }
 	
 	@ViewBuilder
-	private func HeaderView() -> some View {
+    private func HeaderView(geo: GeometryProxy) -> some View {
 		HStack {
-			Text("\(vm.currentLevel.id) 단계")
-				.font(.system(size: ))
-		}
+            Text("\(vm.currentLevel.id) 단계")
+                .font(.title)
+                .fontWeight(.bold)
+                .foregroundStyle(.white)
+                .padding(.leading)
+                .padding(.leading)
+            Spacer()
+            HStack {
+                ForEach(0..<15) { index in
+                    if index < vm.questions.count {
+                        if let didAnswerCorrectly = vm.questions[index].isCorrect {
+                            if didAnswerCorrectly {
+                                Circle()
+                                    .fill(.white)
+                                    .scaleEffect(1.7)
+                                    .overlay {
+                                        Image(systemName: "checkmark")
+                                            .resizable()
+                                            .scaledToFit()
+                                            .scaleEffect(1.5)
+                                            .foregroundStyle(.mint)
+                                    }
+                            } else {
+                                Circle()
+                                    .fill(.gray.opacity(0.5))
+                                    .scaleEffect(1.7)
+                                    .overlay {
+                                        Image(systemName: "circle")
+                                            .resizable()
+                                            .scaledToFit()
+                                            .scaleEffect(1.5)
+                                            .foregroundStyle(.pink)
+                                    }
+                            }
+                        } else {
+                            Circle()
+                                .fill(.gray.opacity(0.5))
+                                .scaleEffect(1.7)
+                                .font(.title)
+                        }//:CONDITIONAL
+                    }//:CONDITIONAL
+                } //:LOOP
+            } //:HSTACK
+            
+            Spacer()
+            
+            Text("\(vm.correctCount) / 15")
+                .font(.title)
+                .fontWeight(.bold)
+                .foregroundStyle(.white)
+                .padding(.trailing)
+                .padding(.trailing)
+            
+            
+		} //:HSTACK
+        .padding(.bottom)
+        .padding(.bottom)
 	}
 	
 	@ViewBuilder
@@ -47,5 +101,8 @@ struct GameView: View {
 }
 
 #Preview {
-    GameView()
+    let vm = GameViewModel()
+    vm.startGame(level: .one)
+    return GameView()
+        .environment(vm)
 }
